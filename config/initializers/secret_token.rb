fa28717c246bd78a4465629a53ed97a227e43c74
Spyros Livathinos <livathinos.spyros@gmail.com>
2010-11-14 14:53:00 +0200

    Initial push.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thinkcactus::Application.config.secret_token = 'b2de12090542782adc4b439efad2a01f49d214928bfd5803d1355e3d17939439d838a4329bee2a736d90179e2b86bd9a15df66e06ba6445997e482326e7ecf15'
