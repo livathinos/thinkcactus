1a00df17d87afd95a62d3725742f19c2e88b12dc
Spyros Livathinos <livathinos.spyros@gmail.com>
2010-11-25 19:40:18 +0200

    Comment section updated, posts are now showing properly. Missing concatenation still on the frontpage. Looking good.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates :body, :presence => true,
+            :length => { :minimum => 5 }
+  validates :author_name, :presence => true,
+            :length => { :minimum => 5}
 end
