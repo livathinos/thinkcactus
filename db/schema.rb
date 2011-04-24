e834039fc6236051e7c9e5a11e34e254798dced9
Spyros Livathinos <livathinos.spyros@gmail.com>
2011-04-24 11:30:21 +0300

    Added categories and tags to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110424072349) do
+ActiveRecord::Schema.define(:version => 20110424082808) do
 
   create_table "admin_users", :force => true do |t|
     t.string   "first_name",       :default => "",    :null => false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20110424072349) do
     t.datetime "updated_at"
     t.string   "tag_one"
     t.string   "tag_two"
+    t.string   "category"
   end
 
   create_table "users", :force => true do |t|
