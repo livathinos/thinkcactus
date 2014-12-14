c232c953578d8f130d39f7ef019e2b8948baf1ae
Spyros Livathinos <livathinos.spyros@gmail.com>
2014-12-14 14:03:13 +0100

    Override ActiveAdmin resource finder with friendly_id

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,5 +1,9 @@
 ActiveAdmin.register Post do
   controller do
+    def find_resource
+      scoped_collection.friendly.find(params[:id])
+    end
+
     def permitted_params
       params.permit!
     end
